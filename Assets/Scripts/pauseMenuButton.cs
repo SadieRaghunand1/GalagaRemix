f0c29d863d87d4fd4ab2a9d55fc17db75d19a78c
SadieRaghunand1 <sadielrowl@outlook.com>
2025-02-11 22:16:35 -0500

    Fix pause menu

--- Assets/Scripts/pauseMenuButton.cs
+++ Assets/Scripts/pauseMenuButton.cs
@@ -2,28 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pauseMenuButton : MonoBehaviour
 {
     public GameObject playImage;
-    private bool menu;
+    public bool menu;
+    public Canvas canvas;
     // opens the how to play section while paused
     public void howPlay()
     {
+        
         if (menu == false)
         {
-            playImage.SetActive(false);
+            playImage.SetActive(true);
             menu = true;
         }
         else if (menu == true)
         {
-            playImage.SetActive(true);
+            playImage.SetActive(false);
             menu = false;
         }
     }
+
+
+    public void ShowHowToPlay()
+    {
+        Debug.Log("Show how to play");
+        //playImage.SetActive(true);
+        canvas.enabled = true;
+        menu = true;
+    }
+
+    public void HideHowToPlay()
+    {
+        Debug.Log("Hide to play");
+        //playImage.SetActive(false);
+        canvas.enabled = false;
+        menu = false;
+    }
+
     public void Start()
     {
-        playImage.SetActive(false);
+       // playImage.SetActive(false);
         menu = false;
     }
 }
